a8f856f2addbecd5b051f618c84fafabd5dbfc5f
EatMyBytes <345links@gmail.com>
2018-05-11 00:38:01 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+$(function() {
+    // $("*").fadeIn(600)
+    screen = {
+        width: window.outerWidth,
+        height: window.outerHeight,
+    }
+    console.log(screen)
+    if (screen.width < 600) {
+
+        style = $("#mobile-sheet")
+        $(style).attr("href", "mobile-style.css")
+
+        setInterval(function() {
+            $("#profilePicture").css(
+                "height", $("#profilePicture").css("width")
+            )
+        }, 1000 / 60)
+    }
+})
